3afb3450948ebe6b70b472b47311e48b316af168
Bhao.Zzo <35212019+zoubohao@users.noreply.github.com>
2025-06-13 20:38:43 +0800

    update v1.0.9.10

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ requires_list = [i.strip() for i in requires_list]
 
 setup(
     name=name,
-    version='1.0.9.7',
+    version='1.0.9.10',
     author="Bohao Zou",
     author_email='csbhzou@comp.hkbu.edu.hk',
     description="The binner to cluster contigs.",
